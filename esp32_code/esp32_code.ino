47b11e531fdb2730fcbd94f943fc817329720fbd
DMUPraveen <dmu.praveen@gmail.com>
2024-07-14 14:19:16 +0530

    Changed the communicating serial port to serial 2

--- esp32_code/esp32_code.ino
+++ esp32_code/esp32_code.ino
@@ -2,36 +2,39 @@
 #include "arbitrary_wave_command.h"
 #include "sine_wave_command.h"
 #include "Command_codes.h"
+#define RXD2 16
+#define TXD2 17
 void serial_cleanup()
 {
-    while (Serial.available())
+    while (Serial2.available())
     {
-        Serial.read();
+        Serial2.read();
     }
 }
 void setup()
 {
+    Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
     Serial.begin(115200);
     auto sig = Signal_Generator();
     while (1)
     {
-        while (!Serial.available())
+        while (!Serial2.available())
         {
             /*
             Busy wait while no data is available
             */
         }
-        uint8_t command = Serial.read(); // read the command code
+        uint8_t command = Serial2.read(); // read the command code
         switch (command)
         {
         case SINE_WAVE_COMMAND:
             /* code */
             run_sine_gen_command(&sig);
-            // Serial.println("Sine wave OK");
+            Serial.println("Sine wave OK");
             break;
         case ARBITRARY_WAVE_COMMAND:
             run_arbitrary_wave_command(&sig);
-            // Serial.println("Arbitrary wave OK");
+            Serial.println("Arbitrary wave OK");
             break;
 
         default:
