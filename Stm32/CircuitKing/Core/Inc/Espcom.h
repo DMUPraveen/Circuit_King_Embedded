14f109bf67b99a0e986835eab3feb50054d67069
DMUPraveen <dmu.praveen@gmail.com>
2024-07-14 13:58:00 +0530

    Transmitting Done

--- Stm32/CircuitKing/Core/Inc/Espcom.h
+++ Stm32/CircuitKing/Core/Inc/Espcom.h
@@ -1,4 +1,5 @@
 #ifndef ESPCOM_H
 #define ESPCOM_H
 #include "usb_driver.h"
+void run_esp_com();
 #endif
--- Stm32/CircuitKing/Core/Src/Communication.c
+++ Stm32/CircuitKing/Core/Src/Communication.c
@@ -1,5 +1,6 @@
 #include "Communication.h"
 #include "Pulse_Measure.h"
+#include "Espcom.h"
 void communication(){
 	if(usb_driver_data_ready==USB_DRIVER_DATA_READY){
 		uint8_t code = usb_driver_data_buffer[0];
@@ -9,6 +10,12 @@ void communication(){
 				break;
 			case PULSE_DURATION_COMMAND:
 				complete_pulse_measure();
+				break;
+			case SINE_WAVE_COMMAND:
+				run_esp_com();
+				break;
+			case ARBITRARY_WAVE_COMMAND:
+				run_esp_com();
 				break;
 				
 		}
